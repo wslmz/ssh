201f28d5a466c9b5364a80007c1cb823d93656e8
wslmz <67459044+wslmz@users.noreply.github.com>
2020-10-01 20:40:23 +0800

    Update ssh-server.sh

--- ssh-server.sh
+++ ssh-server.sh
@@ -1,26 +1,26 @@
-1. #!/bin/bash
-2. if [[ -z "$NGROK_TOKEN" ]]; then
-3.   echo "Please set 'NGROK_TOKEN'"
-4.   exit 2
-5. fi
-6. if [[ -z "$SSH_PASSWORD" ]]; then
-7.   echo "Please set 'SSH_PASSWORD' for user: $USER"
-8.   exit 3
-9. fi
-10. wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-11. unzip ngrok-stable-linux-386.zip
-12. chmod +x ./ngrok
-13. echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | sudo passwd "$USER"
-14. rm -f .ngrok.log
-15. ./ngrok authtoken "$NGROK_TOKEN"
-16. ./ngrok tcp 22 --log ".ngrok.log" &
-17. sleep 10
-18. HAS_ERRORS=$(grep "command failed" < .ngrok.log)
-19. if [[ -z "$HAS_ERRORS" ]]; then
-20.   echo ""
-21.   echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
-22.   echo ""
-23. else
-24.   echo "$HAS_ERRORS"
-25.   exit 4
-26. fi
+#!/bin/bash
+if [[ -z "$NGROK_TOKEN" ]]; then
+  echo "Please set 'NGROK_TOKEN'"
+  exit 2
+fi
+if [[ -z "$SSH_PASSWORD" ]]; then
+  echo "Please set 'SSH_PASSWORD' for user: $USER"
+  exit 3
+fi
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip ngrok-stable-linux-386.zip
+chmod +x ./ngrok
+echo -e "$SSH_PASSWORD\n$SSH_PASSWORD" | sudo passwd "$USER"
+rm -f .ngrok.log
+./ngrok authtoken "$NGROK_TOKEN"
+./ngrok tcp 22 --log ".ngrok.log" &
+sleep 10
+HAS_ERRORS=$(grep "command failed" < .ngrok.log)
+if [[ -z "$HAS_ERRORS" ]]; then
+  echo ""
+  echo "To connect: $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $USER@/" | sed "s/:/ -p /")"
+  echo ""
+else
+  echo "$HAS_ERRORS"
+  exit 4
+  fi
